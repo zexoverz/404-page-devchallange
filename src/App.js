c916017265e95c92c7c4635cada38a5f64f012f1
zexoverz <aryuical@gmail.com>
2020-12-31 18:13:01 +0700

    Fix Responsive

--- src/App.js
+++ src/App.js
@@ -3,37 +3,25 @@ import './App.css';
 import {Typography, Box, Button} from '@material-ui/core';
 import scarecrow from './Scarecrow.png';
 
+
+
+
 function App() {
   return (
     <div id="App">
-      <Box display="flex" flexDirection="col" >
-        <Box>
-        <Typography id="t1" >404 not found</Typography>
-        </Box>
-
-        <Box display="flex" flexDirection="row" m="auto" p={15} >
-          <Box p={1} mr={8} >
+      <Typography id="t1" >404 not found ❌</Typography>
+      <Box minHeight="100vh" display="flex" flexDirection="row" m="auto" flexWrap="wrap" justifyContent="center" alignItems="center"  >
+          <Box >
             <img src={scarecrow} alt="img" id="img"></img>
           </Box>
 
-          <Box p={1} flexDirection="col">
-
-            <Box>
-              <Typography id="t2">I have bad news for you</Typography>
-            </Box>
-
-            <Box>
-              <p id="t3">The page you are looking for might be removed or is temporarily unavailable</p>
-            </Box>
-
-            <Box>
-            <Button id="btn" className="left">Back to homepage</Button>
-            </Box>
+          <Box flexDirection="col" flexWrap="wrap" p='0px 20px' width="300px"> 
+          <Typography id="t2">I have bad news for you</Typography>
+          <p id="t3">The page you are looking for might be removed or is temporarily unavailable</p>
+          <Button id="btn" className="left">Back to homepage</Button>
           </Box>
-        </Box>
-
-        <Typography id="t4">Faisal Firdani@DevChallenges.io</Typography>
       </Box>
+      <Typography id="t4">Faisal Firdani@DevChallenges.io</Typography>
     </div>
   );
 }
